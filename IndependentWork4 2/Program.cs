f173dc78df605134c00608ef0bd516e085c91f42
Андрей Маленьких <mrmaloy-1992@mail.ru>
2022-09-27 12:40:48 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork4 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, s;
+            Console.WriteLine("Ввести размеры прямоуголника A,B и размеры квадрата C");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            s = 0;
+            int x;
+            while ((a - c) >= 0)
+            {
+                a -= c;
+                x = b;
+                while (x - c >= 0)
+
+                    x -= c;
+                ++s;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести последовательность целых чисел, для окончания ввода прописат ноль");
+            int a = 1;
+            int Sum = 0;
+            int b;
+            do
+            {
+                b = Convert.ToInt32(Console.ReadLine());
+                if (b > 0)
+                    Sum += b;
+                else
+                    if (b != 0)
+                    a *= b;
+            }
+            while (b != 0);
+            if (Sum > a)
+                Console.WriteLine("Сумма ({0}) больше произведения ({1})", Sum, a);
+            else
+                if (Sum < a)
+                Console.WriteLine("Произведение ({1}) больше суммы ({0})", Sum, a);
+            else
+                Console.WriteLine("Произведение ({1}) и сумма ({0}) равны", Sum, a);
+            Console.ReadKey();
+        }
+    }
+}
